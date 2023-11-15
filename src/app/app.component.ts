6b761e4925f4b6e2e0b1a14b62952cb3305d6382
Lars Kleen <lkleen@q4v2l4k9gr.fritz.box>
2023-11-15 14:26:47 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { TodoItem } from "./todoItems";
+import { TodoList } from "./todoList";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  private list = new TodoList(
+    "Peter", [
+      new TodoItem("foo", true),
+      new TodoItem("bar"),
+      new TodoItem("baz"),
+    ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items.filter(item => !item.complete);
+  }
+
+  addItem (item: string) {
+    this.list.addItem (item);
+  }
+}
