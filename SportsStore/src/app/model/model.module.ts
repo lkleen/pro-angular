d120efc9054a83b5fead4cd6d2854fd516fa3a70
Lars Kleen <lkleen@q4v2l4k9gr.fritz.box>
2023-11-20 14:32:36 +0100

    acquire data via REST

--- SportsStore/src/app/model/model.module.ts
+++ SportsStore/src/app/model/model.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from "@angular/core";
-import { ProductRepository } from "./product.repository";
-import { StaticDataSource } from "./static.datasource";
-import { Cart } from "./cart.model";
-import { Order } from "./order.model";
-import { OrderRepository } from "./order.repository";
+import {NgModule} from "@angular/core";
+import {ProductRepository} from "./product.repository";
+import {StaticDataSource} from "./static.datasource";
+import {Cart} from "./cart.model";
+import {Order} from "./order.model";
+import {OrderRepository} from "./order.repository";
+import {RestDataSource} from "./rest.datasource";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
+  imports: [HttpClientModule],
   providers: [
     ProductRepository,
     StaticDataSource,
     Cart,
     Order,
-    OrderRepository
+    OrderRepository,
+    {provide: StaticDataSource, useClass: RestDataSource}
   ]
 })
-export class ModelModule {}
+export class ModelModule {
+}
 
